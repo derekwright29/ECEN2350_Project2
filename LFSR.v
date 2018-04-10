4ecb2a20712682c4a656a0a5453f6e43ff5c6634
Derek <derek.wright@colorado.edu>
2018-04-10 10:45:51 -0600

    Change LFSR settings

--- LFSR.v
+++ LFSR.v
@@ -1,6 +1,6 @@
 module LFSR(enable, LF_shift_reg, ready);
    input enable;
-   output [9:0] LF_shift_reg;
+   output [11:0] LF_shift_reg;
    output 	ready;
    
    wire 	in_shift;
@@ -9,7 +9,7 @@ module LFSR(enable, LF_shift_reg, ready);
      begin
 	in_shift = LF_shift_reg[9] ^ LF_shift_reg[5];
 	LF_shift_reg = {LF_shift_reg[8:0],in_shift};
-	if (LF_shift_reg < 50)
+	if (LF_shift_reg < 500)
 	  ready = 0;
 	else
 	  ready = 1;
