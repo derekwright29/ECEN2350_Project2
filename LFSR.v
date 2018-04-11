bf140b049433a715a076497af8f1f4fa25ff7c4e
Derek <derek.wright@colorado.edu>
2018-04-10 22:15:12 -0600

    LFSR Research

--- LFSR.v
+++ LFSR.v
@@ -1,9 +1,15 @@
+//check this out: https://en.wikipedia.org/wiki/Linear-feedback_shift_register
+// Try to find a 12-bit Fib LFSR... 
+// would be good.
+//The 16-bit Fibonnaci LFSR can go through all 65535 states. Good one. 
+// BUT, 65 seconds is probably toooo long.
+
 module LFSR(enable, LF_shift_reg, ready);
    input enable;
    output [11:0] LF_shift_reg;
    output 	ready;
    
-   wire 	in_shift;
+   reg 		in_shift;
    
    if (enable)
      begin
@@ -11,7 +17,7 @@ module LFSR(enable, LF_shift_reg, ready);
 	LF_shift_reg = {LF_shift_reg[8:0],in_shift};
 	if (LF_shift_reg < 500)
 	  ready = 0;
-	else
+	else if (LF_shift_reg > 5000
 	  ready = 1;
      end
 endmodule // LFSR
